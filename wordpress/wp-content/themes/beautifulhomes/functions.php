f01808fe694835938e5aaf1b49c3b4c9867ce52d
Fellype <fellypenc@gmail.com>
2015-06-23 16:17:36 -0300

    Trying to disable jQuery UI

--- wordpress/wp-content/themes/beautifulhomes/functions.php
+++ wordpress/wp-content/themes/beautifulhomes/functions.php
@@ -1,4 +1,16 @@
 <?php
+
+/**
+ * Dequeue the jQuery UI script.
+ *
+ * Hooked to the wp_print_scripts action, with a late priority (100),
+ * so that it is after the script was enqueued.
+ */
+function wpdocs_dequeue_script() {
+   wp_dequeue_script( 'jquery-ui-core' );
+}
+add_action( 'wp_print_scripts', 'wpdocs_dequeue_script', 100 );
+
 require_once (TEMPLATEPATH . '/framework/theme-files.php');
 $theme = new wt_themeFiles();
 $theme->wt_init(array(
